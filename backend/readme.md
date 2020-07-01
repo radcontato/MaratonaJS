d4f07a39b55a26a645fd31a437a9d3a093f2240d
Roberto Albano <rad.contato@gmail.com>
2020-06-30 22:26:11 -0300

    Nodemon configured and auth controller created

--- backend/readme.md
+++ backend/readme.md
@@ -2,3 +2,9 @@ inicio
 
 npm init
 npm install --save express
+node index.js
+
+git init
+
+
+npm install --save nodemon
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const authController = require('./controllers/auth');
 const app = express();
 
+
+app.use('/auth', authController);
+
 app.get('/', (req, res) => {
-
     return res.json('Api running...')
-
 })
 
 app.listen(3001, () => {
